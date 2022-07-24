27d19ab2b42f4eb26589924e5b5e3872f01e23f0
Marcos Guerreros Ocón <guerrerosoconm@gmail.com>
2022-07-24 18:39:57 +0200

    v.2

--- app/src/main/java/com/example/magictemporador/MainActivity.java
+++ app/src/main/java/com/example/magictemporador/MainActivity.java
@@ -12,91 +12,115 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView txtTiempo, txtVidas;
-    Button iniciar, reiniciar;
+    Button iniciar, reiniciar, pausar;
+    CountDownTimer contador;
+    long tiempoInicial, vidasIniciales;
+    boolean flagEnableIniciado, flagEnablePausa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final long[] tiempoActual = {30};
-
         txtVidas = findViewById(R.id.txtValorVidas);
         txtTiempo = findViewById(R.id.txtValorTiempo);
+
         iniciar = (Button) findViewById(R.id.btnIniciar);
         reiniciar = (Button) findViewById(R.id.btnReiniciar);
+        pausar = (Button) findViewById(R.id.btnPausar);
+
+        tiempoInicial = 30;
+        vidasIniciales = 1;
+
+        setFlagEnableIniciado(true);
+        setFlagEnablePausa(false);
+
+
+        iniciarContador();
 
         iniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String valorTiempo = txtTiempo.getText().toString();
-                int valorVidas = Integer.parseInt(txtVidas.getText().toString());
-                long segundos = Long.parseLong(valorTiempo.substring(valorTiempo.indexOf(":") + 1));
-                long minutos = Long.parseLong(valorTiempo.substring(0, valorTiempo.indexOf(":")));
-                long tiempoTotal = segundos + minutos * 60;
-
-                new CountDownTimer(tiempoTotal * 1000, 1000) {
-                    @Override
-                    public void onTick(long l) {
-                        long minutos = (l / 1000) / 60;
-                        long segundos = (l / 1000) - minutos;
-
-                        formatearTiempo(minutos, segundos);
-                    }
-
-                    @Override
-                    public void onFinish() {
-                        long nuevoTiempo = tiempoTotal + 10;
-                        long minutos = nuevoTiempo / 60;
-                        nuevoTiempo = nuevoTiempo - minutos * 60;
-                        long segundos = nuevoTiempo;
-                        int nuevasVidas = valorVidas + 1;
-
-                        formatearTiempo(minutos, segundos);
-
-                        tiempoActual[0] = tiempoTotal + 10;
-                        txtVidas.setText(nuevasVidas + "");
-                    }
-                }.start();
-
+                setFlagEnableIniciado(false);
+                pausar.setEnabled(true);
+                iniciarTemporizador();
             }
-
         });
 
         reiniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new CountDownTimer(tiempoActual[0] * 1000, 1000) {
-                    @Override
-                    public void onTick(long l) {
-                        long minutos = (l / 1000) / 60;
-                        long segundos = (l / 1000) - minutos;
+                reiniciarTemporizador();
+            }
+        });
 
-                        formatearTiempo(minutos, segundos);
-                    }
-
-                    @Override
-                    public void onFinish() {
-                        long nuevoTiempo = tiempoActual[0] + 10;
-                        long minutos = nuevoTiempo / 60;
-                        nuevoTiempo = nuevoTiempo - minutos * 60;
-                        long segundos = nuevoTiempo;
-
-                        formatearTiempo(minutos, segundos);
-
-                        tiempoActual[0] = tiempoActual[0] + 10;
-                    }
-                };
-                long minutos = tiempoActual[0] / 60;
-                tiempoActual[0] = tiempoActual[0] - minutos * 60;
-                long segundos = tiempoActual[0];
-
-                formatearTiempo(minutos, segundos);
+        pausar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setFlagEnableIniciado(true);
+                pausar.setEnabled(false);
+                pausarTemporizador();
             }
         });
     }
 
+    private void iniciarContador() {
+
+        long tiempo = tiempoInicial;
+        long minutos = tiempo / 60;
+        long segundos = tiempo - minutos * 60;
+        long tiempoTotal = segundos + minutos * 60;
+
+
+        contador = new CountDownTimerPausable(tiempoTotal * 1000, 1000) {
+
+            @Override
+            public void onTick(long l) {
+                long tiempo = l / 1000;
+                long minutos = tiempo / 60;
+                long segundos = tiempo - minutos * 60;
+
+                formatearTiempo(minutos, segundos);
+            }
+
+            @Override
+            public void onFinish() {
+                tiempoInicial = tiempoInicial + 10;
+                long minutos = tiempoInicial / 60;
+                long segundos = tiempoInicial - minutos * 60;
+
+                formatearTiempo(minutos, segundos);
+                vidasIniciales++;
+
+                txtVidas.setText(vidasIniciales + "");
+
+                setFlagEnableIniciado(true);
+
+
+            }
+        };
+    }
+
+    private void pausarTemporizador() {
+        contador.cancel();
+    }
+
+    private void reiniciarTemporizador() {
+        if (isFlagEnableIniciado()){
+            setFlagEnableIniciado(false);
+        }
+        setFlagEnablePausa(true);
+
+        contador.cancel();
+        contador.start();
+    }
+
+    private void iniciarTemporizador() {
+        contador.start();
+    }
+
     private void formatearTiempo(long minutos, long segundos) {
+
         String tiempo;
         if (minutos < 10) {
             tiempo = "0" + minutos;
@@ -111,7 +135,25 @@ public class MainActivity extends AppCompatActivity {
             tiempo = tiempo + segundos + "";
         }
 
+
         txtTiempo.setText(tiempo);
     }
 
+    private boolean isFlagEnableIniciado() {
+        return flagEnableIniciado;
+    }
+
+    private void setFlagEnableIniciado(boolean valor) {
+        flagEnableIniciado = valor;
+        iniciar.setEnabled(valor);
+    }
+
+    private boolean isFlagEnablePausa() {
+        return flagEnablePausa;
+    }
+
+    private void setFlagEnablePausa(boolean valor) {
+        flagEnablePausa = valor;
+        pausar.setEnabled(valor);
+    }
 }
